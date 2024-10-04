912c5afe03da7875aa39e9c4112dd306e26901b1
Lucas Guevremont <lucasg25@icloud.com>
2024-10-04 10:12:22 -0500

    commit

--- JS/Objects/bank_account/bank.js
+++ JS/Objects/bank_account/bank.js
@@ -1,4 +1,4 @@
-const BankAccount = (balance) =>
+const BankAccount2 = (balance) =>
     (function (copyBalance) {
         let balance = copyBalance; // Variable "privée"
         const createMessage = function () { // Méthode "privée"
@@ -26,15 +26,37 @@ const BankAccount = (balance) =>
         };
     })(balance);
 
-// TODO
-// class BankAccount { }
+class BankAccount {
+    #balance = 0;
+    #createMessage() {
+        console.log("Compte créé avec un montant de " + this.#balance + "$");
+    }
+    constructor(balance) {
+        this.#balance = balance;
+        this.#createMessage();
+    }
+    checkBalance() {
+        return this.#balance;
+    }
+    withdraw(amount) {
+        if (amount > this.#balance) return "Fonds insuffisants";
+        this.balance -= amount;
+        return this.#balance;
+    }
+    deposit(amount) {
+        if (amount < 0) return "Impossibe de déposer un montant négatif";
+        this.balance += amount;
+        return this.#balance;
+    }
 
-const firstAccount = BankAccount(100); // "Compte créé avec un montant de 100$"
+};
+
+const firstAccount = new BankAccount(100); // "Compte créé avec un montant de 100$"
 console.log(firstAccount.balance); // undefined
 console.log(firstAccount.checkBalance()); // 100
 console.log(firstAccount.withdraw(20)); // 80
 console.log(firstAccount.deposit(30)); // 110
 console.log(firstAccount.createMessage); // undefined
-const secondAccount = BankAccount(20); // "Compte créé avec un montant de 20$"
+const secondAccount = new BankAccount(20); // "Compte créé avec un montant de 20$"
 console.log(secondAccount.withdraw(30)); // "Fonds insuffisants"
 console.log(secondAccount.deposit(-20)); // "Impossible de déposer un montant négatif"
